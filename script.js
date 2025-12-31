21ec80dfc8ce9a433ae02ae495cea90ec18c6847
Geovanna  Borges <borgesgeovanna16@gmail.com>
2025-12-30 23:37:58 -0300

    :tada

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  // Pegar a tag img
+  const img = document.querySelector("#profile img")
+  // Substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //se não tiver light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("alt", "Foto de um intardecer")
+  } else {
+    img.setAttribute("alt", "Foto de um anoitecer")
+  }
+}
